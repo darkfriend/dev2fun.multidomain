9c26f9dc0947f7ba176ea6fa642699da24181ccf
darkfriend <maslov.tlt@gmail.com>
2020-02-19 22:47:05 +0300

    added event OnBeforeSeoSetCityName on {=get_city}

--- utf8/dev2fun.multidomain/classes/general/TemplateSeo.php
+++ utf8/dev2fun.multidomain/classes/general/TemplateSeo.php
@@ -10,19 +10,24 @@ namespace Dev2fun\MultiDomain;
 
 class TemplateSeo extends \Bitrix\Iblock\Template\Functions\FunctionBase
 {
-	/**
-	 * Обработчик события на вход получает имя требуемой функции
-	 * @example {=get_city}
-	 * @param \Bitrix\Main\Event $event
-	 * @return string
-	 */
-	public static function EventHandler(\Bitrix\Main\Event $event) {
-		$parameters = $event->getParameters();
-		$functionName = $parameters[0];
-		if ($functionName === "get_city") {
-			return \Dev2fun\MultiDomain\Base::GetCurrentDomain()['UF_NAME'];
-		} else {
-			return '';
-		}
-	}
+    /**
+     * Обработчик события на вход получает имя требуемой функции
+     * @param \Bitrix\Main\Event $event
+     * @return string
+     * @example {=get_city}
+     */
+    public static function EventHandler(\Bitrix\Main\Event $event)
+    {
+        $parameters = $event->getParameters();
+        $functionName = $parameters[0];
+        if ($functionName === "get_city") {
+            $currentDomain = \Dev2fun\MultiDomain\Base::GetCurrentDomain();
+            $cityName = $currentDomain['UF_NAME'];
+            foreach (GetModuleEvents('dev2fun.multidomain', "OnBeforeSeoSetCityName", true) as $arEvent)
+                ExecuteModuleEventEx($arEvent, [&$cityName, $currentDomain]);
+            return $cityName;
+        } else {
+            return '';
+        }
+    }
 }
